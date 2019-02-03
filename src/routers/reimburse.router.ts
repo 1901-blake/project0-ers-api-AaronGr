c452ab71e5bd953a07bda00b3a43224e67660f5b
Aaron Gravelle <agrav12825@gmail.com>
2019-02-03 13:55:48 -0500

    Add authentication middleware to reimbursement router

--- src/routers/reimburse.router.ts
+++ src/routers/reimburse.router.ts
@@ -2,31 +2,32 @@ import express from 'express';
 import * as Data from '../data';
 import { STATUS_CODES } from 'http';
 import { ReimbursementDao } from '../dao/reimburse-dao';
+import { authMiddleware } from '../middleware/auth.middleware';
 
 export const reimburseRouter = express.Router();
 const reimbursmentDao = new ReimbursementDao();
 
-reimburseRouter.get('/status/:statusId', (req, res) => {
+reimburseRouter.get('/status/:statusId', [authMiddleware(['admin', 'financial-manager']), (req, res) => {
     reimbursmentDao.getReimbursementsByStatusId(+req.params.statusId)
     .then(reimbursementArray => res.status(200).send(reimbursementArray));
-});
+}]);
 
-reimburseRouter.get('/author/userId/:userId', (req, res) => {
+reimburseRouter.get('/author/userId/:userId', [authMiddleware(['admin', 'financial-manager', 'associate']), (req, res) => {
     reimbursmentDao.getReimbursementsByUserId(+req.params.userId)
     .then(reimbursementArray => res.status(200).send(reimbursementArray));
-  });
+  }]);
 
-reimburseRouter.post('', (req, res) => {
+reimburseRouter.post('', [authMiddleware(['admin', 'financial-manager', 'associate']), (req, res) => {
     if (req.body.reimbursementId === 0) {
         reimbursmentDao.submitReimbursement(req.body)
         .then( reimb => res.status(201).send(reimb));
     } else {
          res.sendStatus(400);
     }
-  });
+  }]);
 
 // To update the reimbursement reimbursementId must be present
-reimburseRouter.patch('', (req, res) => {
+reimburseRouter.patch('', [authMiddleware(['admin', 'financial-manager']), (req, res) => {
     reimbursmentDao.getReimbursementById(req.body.reimbursementId).then( reimb => {
       const props: string[] = Object.keys(reimb);
       props.forEach( prop => {
@@ -40,4 +41,4 @@ reimburseRouter.patch('', (req, res) => {
           res.json(reimbForUpdating);
       });
     });
-});
+}]);
